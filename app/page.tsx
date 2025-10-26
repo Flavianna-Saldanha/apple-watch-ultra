2d32d021eb0a2488d3eb2b355dcf0f2e1911f80e
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2025-10-26 19:27:30 -0300

    add seção `hero`

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+import { Hero } from "@/src/components/hero/Hero";
+
 export default function Home() {
-  return <div>...</div>;
+  return (
+    <div>
+      <header>
+        <a href="#">
+          <img src="/icone.png" alt="logo da iPhone" />
+        </a>
+        <a href="#">
+          <img
+            src="/assets/icons/shopping-bag.svg"
+            alt="logo de carrinho de compras"
+          />
+          <span>1</span>
+        </a>
+      </header>
+
+      <Hero />
+    </div>
+  );
 }
---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,17 @@
+export const Hero = () => {
+  return (
+    <div className="hero">
+      <div>
+        <h1>
+          Apple Watch Ultra 3
+          <div>
+            <span>imparável</span>
+            <span>recordista </span>
+            <span>campeão</span>
+            <span>imparável</span>
+          </div>
+        </h1>
+      </div>
+    </div>
+  );
+};
