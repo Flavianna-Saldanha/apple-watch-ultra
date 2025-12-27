c7ad870a340f793b025520faab74d6088212fb41
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2025-12-27 00:19:18 -0300

    adicionando `nova seção`

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Hero } from "@/src/components/hero/Hero";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -38,6 +39,40 @@ export default function Home() {
             <img src="/assets/banner.png" alt="" width={140} />
             <img src="/assets/banner.png" alt="" width={140} />
           </div>
+        </div>
+      </section>
+
+      <section className="flex flex-col items-center gap-6 text-center">
+        <div>
+          <Image
+            src="/assets/hero/hero.png"
+            alt="imagem de um iPhone"
+            width={839}
+            height={644}
+          />
+        </div>
+
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex items-center gap-2">
+            <Image
+              src="/icone.png"
+              alt="logo Apple Watch"
+              width={30}
+              height={30}
+            />
+            <p className="uppercase">Watch Ultra 3</p>
+          </div>
+
+          <p className="text-lg font-semibold">Imparável.</p>
+        </div>
+
+        <div className="h-20 flex items-center w-full justify-around bg-gray-100">
+          <p className="font-bold w-52 text-start text-gray-800">
+            A partir de R$ 874,92/mês ou R$ 10.499*
+          </p>
+          <button className="text-white h-9 w-24 bg-blue-500 rounded-full">
+            Comprar
+          </button>
         </div>
       </section>
     </div>
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,6 +1,6 @@
 export const Hero = () => {
   return (
-    <div className="hero max-w-7xl w-full py-10 px-8 mx-auto flex flex-col-reverse items-center gap-10 overflow-hidden xl:flex-row xl:justify-between">
+    <div className="hero max-w-7xl w-full py-10 px-8 mx-auto flex flex-col-reverse items-center gap-10 overflow-visible xl:overflow-hidden xl:flex-row xl:justify-between">
       <div className="inicio flex flex-col items-center gap-7 xl:items-start">
         <h1 className="max-w-2xl text-center xl:text-start">
           Apple Watch Ultra 3
@@ -11,10 +11,12 @@ export const Hero = () => {
             <span className="text-(--snitap-sky-mid)">imparável</span>
           </div>
         </h1>
+
         <div className="buttons flex flex-col gap-8 mt-12 xl:flex-row">
           <a href="#" className="button buy shadow">
             <img src="/assets/icons/shopping-bag.svg" alt="" /> COMPRAR AGORA
           </a>
+
           <a href="#" className="button play">
             <span className="shadow">
               <img src="/assets/icons/play.svg" alt="" />
@@ -23,6 +25,7 @@ export const Hero = () => {
           </a>
         </div>
       </div>
+
       <div className="block w-full max-w-96 h-[420px]">
         <div className="content w-full h-full relative">
           <img src="/assets/hero/Ellipse.png" alt="" />
