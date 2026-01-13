bb73e09bb38f075b25156b357934f65fad139e9d
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2026-01-13 10:51:45 -0300

    add quebras de linha

--- src/data/ExplorandoList.ts
+++ src/data/ExplorandoList.ts
@@ -10,24 +10,24 @@ export const ExplorandoList: ExplorandoType[] = [
     id: 2,
     img: "assets/icons/2.png",
     description:
-      "ela Retina Sempre Ativa com amplo ângulo de visão. Até 3.000 nits de brilho. Mais brilhante quando inclinada3",
+      "Tela Retina Sempre Ativa com amplo ângulo de visão. \nAté 3.000 nits de brilho. \nMais brilhante quando inclinada",
   },
   {
     id: 3,
     img: "assets/icons/3.png",
     description:
-      "Chip S10. Gestos de toque duplo e rotação do pulso. Siri. Busca Precisa para iPhone21",
+      "Chip S10. \nGestos de toque duplo e rotação do pulso. Siri. \nBusca Precisa para iPhone",
   },
   {
     id: 4,
     img: "assets/icons/4.png",
     description:
-      "App ECG15. Notificações de frequência cardíaca alta e baixa. Notificações de ritmo cardíaco irregular16. Notificações de preparo cardiovascular baixo. App Oxigênio no Sangue. Qualidade do Sono. Notificações de apneia do sono13",
+      "App ECG. \nNotificações de frequência cardíaca alta e baixa. \nNotificações de ritmo cardíaco irregular. \nNotificações de preparo cardiovascular baixo. \nApp Oxigênio no Sangue. \nQualidade do Sono. \nNotificações de apneia do sono",
   },
   {
     id: 5,
     img: "assets/icons/5.png",
     description:
-      "Até 42 horas. Até 72 horas no Modo Pouca Energia. Recarga rápida",
+      "Até 42 horas. \nAté 72 horas no Modo Pouca Energia. \nRecarga rápida",
   },
 ];
